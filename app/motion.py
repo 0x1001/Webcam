6ddb737cb197e1e795c025475aac487c8f4398ec
Damian Nowok <damian.nowok@gmail.com>
2015-03-04 20:15:35 +0100

    Change to motion detection algorithm. Checks also angles.

--- app/motion.py
+++ app/motion.py
@@ -3,6 +3,8 @@ import picamera.array
 _MAGNITUDE = 10      # If there're more than 10 vectors with a magnitude greater
 _VECTOR_COUNT = 60  # than 60, then say we've detected motion
 
+_ANGLE = 10    # Motion angle differenece + or - from 90.
+
 
 class Motion(picamera.array.PiMotionAnalysis):
     def __init__(self, *args, **kwargs):
@@ -15,12 +17,21 @@ class Motion(picamera.array.PiMotionAnalysis):
     def analyse(self, a):
         import numpy as np
 
-        a = np.sqrt(
+        r = np.sqrt(
             np.square(a['x'].astype(np.float)) +
             np.square(a['y'].astype(np.float))
             ).clip(0, 255).astype(np.uint8)
 
-        if (a > _VECTOR_COUNT).sum() > _MAGNITUDE:
+        phi = ((np.arctan2(
+              a['y'].astype(np.float),
+              a['x'].astype(np.float)) +
+              np.pi) * 180 / (2 * np.pi)
+              ).clip(0, 255).astype(np.uint8)
+
+        if ((r > _VECTOR_COUNT).sum() > _MAGNITUDE and
+            ((phi > _VECTOR_COUNT).sum() > 90 + _ANGLE or
+            (phi > _VECTOR_COUNT).sum() > 90 - _ANGLE)):
+
             self._event.set()
 
     def event(self):
