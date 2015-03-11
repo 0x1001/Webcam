46b6762c274758caa206becf311f25946135c221
Damian Nowok <damian.nowok@gmail.com>
2015-03-11 20:29:57 +0100

    Adding empty db and first version of install.sh script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#  Installs Web Camera third party software
+#
+
+# General update of package database
+sudo apt-get -y update
+
+# Python pip
+sudo apt-get install -y python-pip
+
+# Django
+sudo pip install django
+
+# Pydev
+sudo apt-get install -y python-dev
+
+# PiCamera
+sudo apt-get install -y python-picamera
+
+# PIL
+sudo apt-get install python-imaging
+
+# Gevent
+sudo pip install gevent
