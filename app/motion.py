d5cd9db959c6b07158d2fa5d725be003a23c4a37
Damian Nowok <damian.nowok@gmail.com>
2015-04-27 19:33:31 +0200

    Change in motion algorith to eliminate low light fluctuation

--- app/motion.py
+++ app/motion.py
@@ -1,12 +1,6 @@
 import picamera.array
 
-_MAGNITUDE = 1  # Motion vector magnitude. Range: 0 - 255
-_MIN_SAMPLES = 0.5  # min samples with motion. Range: 0% - 100%
-_MAX_SAMPLES = 30  # max samples with motion. Range: 0% - 100%
-_MIN_MOTION_FRAMES = 3  # How many frames have to see motion before trigger motion detected
-
-_MIN_SAMPLES *= 0.01
-_MAX_SAMPLES *= 0.01
+_MIN_SAMPLES = 4  # min samples with motion. Range: 0% - 100%
 
 
 class Motion(picamera.array.PiMotionAnalysis):
@@ -15,8 +9,9 @@ class Motion(picamera.array.PiMotionAnalysis):
 
         super(Motion, self).__init__(*args, **kwargs)
 
-        self._frames_with_motion = 0
         self._event = threading.Event()
+        self._last = None
+        self._min_samples = None
 
     def analyse(self, a):
         import numpy as np
@@ -24,23 +19,19 @@ class Motion(picamera.array.PiMotionAnalysis):
         r = np.sqrt(
             np.square(a['x'].astype(np.float)) +
             np.square(a['y'].astype(np.float))
-            ).clip(0, 255).astype(np.uint8)
+            ).clip(0, 1)
 
-        samples = len(r) * len(r[0])  # Size of frame. Ex: 81 x 40 for recording resolution 1280 x 720.
+        if self._min_samples is None:
+            # Ex: 81 x 40 (for recording resolution 1280 x 720) x min samples in percent
+            self._min_samples = len(r) * len(r[0]) * _MIN_SAMPLES * 0.01
 
-        samples_min = samples * _MIN_SAMPLES  # min number of samples with motion magnitue greater than _MAGNITUDE
-        samples_max = samples * _MAX_SAMPLES  # max number of samples with motion magnitue greater than _MAGNITUDE
+        if self._last is not None:
+            diff = np.subtract(self._last, r)
 
-        motion_samples = (r > _MAGNITUDE).sum()  # Finds number of samples with motion magnitue greater than _MAGNITUDE
-
-        if motion_samples > samples_min and motion_samples < samples_max:
-            self._motion_frames += 1
-
-            if self._motion_frames == _MIN_MOTION_FRAMES:  # Checks if motion was also detected on previous frames
-                self._motion_frames = 0
+            if (diff == 1).sum() > self._min_samples or (diff == -1).sum() > self._min_samples:
                 self._event.set()
-        else:
-            self._motion_frames = 0
+
+        self._last = r
 
     def event(self):
         return self._event
