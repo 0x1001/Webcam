8a97a77d3dafb2f4dd69fb70fd31399cb5975148
Damian Nowok <damian.nowok@gmail.com>
2015-04-29 00:43:54 +0200

    Motion algorithm improvements

--- app/motion.py
+++ app/motion.py
@@ -7,43 +7,70 @@ _SKIP_FRAMES = 4
 class Motion(picamera.array.PiMotionAnalysis):
     def __init__(self, *args, **kwargs):
         import threading
+        import Queue
 
         super(Motion, self).__init__(*args, **kwargs)
 
         self._event = threading.Event()
+
+        self._processing_queue = Queue.Queue(1)
+
+        self._processing_thread = threading.Thread(target=self._processing)
+        self._processing_thread.setDaemon(True)
+        self._processing_thread.start()
+
         self._skip = 0
 
-    def analyse(self, a):
-        import threading
+    def event(self):
+        return self._event
 
+    def reset(self):
+        self._event.clear()
+
+    def analyse(self, a):
+        import Queue
+
+        if not self._skip_frame() and not self._event.is_set():
+            try:
+                self._processing_queue.put(a, block=False)
+            except Queue.Full:
+                pass
+
+    def _skip_frame(self):
         if self._skip == _SKIP_FRAMES:
             self._skip = 0
+            return False
         else:
             self._skip += 1
-            return
+            return True
 
-        t = threading.Thread(target=self._analyse_thread, args=(a, ))
-        t.setDaemon(True)
-        t.start()
-
-    def _analyse_thread(self, a):
+    def _processing(self):
         import numpy as np
 
-        r = np.sqrt(
-            np.square(a['x'].astype(np.float)) +
-            np.square(a['y'].astype(np.float))
-            ).clip(0, 255)  # Magnitude: 0 to 255
+        while True:
+            a = self._processing_queue.get()
 
-        phi = (np.arctan2(
-              a['y'].astype(np.float),
-              a['x'].astype(np.float)) +
-              np.pi) * 180 / (2 * np.pi)  # Angle degree: -90 to 90.
+            r = np.sqrt(
+                np.square(a['x'].astype(np.float)) +
+                np.square(a['y'].astype(np.float))
+                ).clip(0, 255)  # Magnitude: 0 to 255
 
+            phi = (np.arctan2(
+                  a['y'].astype(np.float),
+                  a['x'].astype(np.float)) +
+                  np.pi) * 180 / (2 * np.pi)  # Angle degree: -90 to 90.
+
+            if self._find_motion(r, phi):
+                self._event.set()
+
+    def _find_motion(self, r, phi):
         for x in range(len(r)):
             for y in range(len(r[0])):
                 if r[x][y] > _THRESHOLD:
                     if self._check_adjacent(x, y, r, phi):
-                        self._event.set()
+                        return True
+
+        return False
 
     def _check_adjacent(self, x, y, r, phi):
         mag_min = r[x][y] - 2
@@ -64,9 +91,3 @@ class Motion(picamera.array.PiMotionAnalysis):
                         hits += 1
 
         return hits >= 5
-
-    def event(self):
-        return self._event
-
-    def reset(self):
-        self._event.clear()
